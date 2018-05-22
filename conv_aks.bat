61129e642e7f40fd127c30644d64ea8642ad6874
ggn <ggn.dbug@gmail.com>
2018-05-22 10:13:53 +0300

    Added event conversion script by XiA

---
+++ conv_aks.bat
@@ -0,0 +1,19 @@
+@echo off
+rem ####################################################################
+rem ## Convert script
+rem ##
+rem ## Usage:
+rem ##   conv_aks source.aks dest_stub
+rem ##
+rem ## Example:
+rem ##   conv_aks knightmare.aks tunes\knightmare
+rem ##   # outputs:
+rem ##   #   knightmare.aky.s
+rem ##   #   knightmare.events.words.s
+
+SongToAky -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -reladr -spomt %1 %2.aky.s
+SongToEvents -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -spomt %1 %2.events.words.s
+sed -i -e "s/dc\.b/dc.w/gI" -e "s/dc\.w Events_/dc.l Events_/gI" %2.events.words.s
+echo.
+echo.
+
