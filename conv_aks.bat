8a27c8d52b7a3cb0d43682fc3ce4367da5ea5c28
ggn <ggn.dbug@gmail.com>
2018-05-23 11:49:31 +0300

    Make conv_aks.bat a bit more user friendly

--- conv_aks.bat
+++ conv_aks.bat
@@ -11,6 +11,16 @@ rem ##   # outputs:
 rem ##   #   knightmare.aky.s
 rem ##   #   knightmare.events.words.s
 
+if "%1"=="" (
+	echo usage: conv_aks source.aks dest_stub
+    exit /b
+)
+if "%2"=="" (
+	echo usage: conv_aks source.aks dest_stub
+    exit /b
+)
+
+
 SongToAky -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -reladr -spomt %1 %2.aky.s
 SongToEvents -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -spomt %1 %2.events.words.s
 sed -i -e "s/dc\.b/dc.w/gI" -e "s/dc\.w Events_/dc.l Events_/gI" %2.events.words.s
