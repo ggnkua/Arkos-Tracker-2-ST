d95f671823aebcf73486a8c00d13fba8bcacb1e2
ggn <ggn.dbug@gmail.com>
2018-05-25 23:08:33 +0300

    Added malfeasant sed command to convert aky .s file to something vasm can eat

--- conv_aks.bat
+++ conv_aks.bat
@@ -22,6 +22,7 @@ if '%2'=='' (
 
 
 SongToAky -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -reladr -spomt %1 %2.aky.s
+sed -e "s/ + /+/gI" -e "s/ - /-/gI" -e "s/, /,/gI" -e "s/dc.b\(.*\); Duration./dcbx\1; Duration./gI" -e "s/dc.b 8\t; Loop/dcbx 8\t; Loop/gI" -e "s/dc.b 8$/dcbx 8/gI" %1 > %2.aky_vasm.s
 SongToEvents -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -spomt %1 %2.events.words.s
 sed -i -e "s/dc\.b/dc.w/gI" -e "s/dc\.w Events_/dc.l Events_/gI" %2.events.words.s
 echo.
