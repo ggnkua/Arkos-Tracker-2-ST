a769003354457ccc4323e1fd41db9ea366587ce2
ggn <ggn.dbug@gmail.com>
2020-05-21 23:33:15 +0300

    Updated tracker and tools to a8. Added horrible hack for endianess issues (Software period is exported byte swapped)

--- conv_aks.bat
+++ conv_aks.bat
@@ -11,16 +11,19 @@ rem ##   # outputs:
 rem ##   #   knightmare.aky.s
 rem ##   #   knightmare.events.words.s
 
-if '%1'=='' (
-	echo usage: conv_aks source.aks dest_stub
-    exit /b
-)
-if '%2'=='' (
-	echo usage: conv_aks source.aks dest_stub
-    exit /b
-)
-
+if '%1'=='' goto :USAGE
+if not '%2'=='' goto :PARAMS_OK
+:USAGE
+echo usage: conv_aks source.aks dest_stub
+exit /b
+:PARAMS_OK
 
 bin\SongToAky -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -reladr -spomt %1 %2.aky.s
 bin\SongToEvents -adr 0 -spadr ; --sourceProfile 68000 -sppostlbl ":" -spomt %1 %2.events.words.s
+
+rem Take care of endianess swap. Seems to be required for versions at least 2.0.0a8 and later
+rem Do not use for earlier versions!
+sed -i -e "s/\( *dc.b \)\([[:digit:]]\+\), \([[:digit:]]\+\)/\1\3,\2/gI" %2.aky.s
+
+rem Convert event values to word size and labels to longwords
 bin\sed -i -e "s/dc\.b/dc.w/gI" -e "s/dc\.w Events_/dc.l Events_/gI" %2.events.words.s
--- sndh.sh
+++
@@ -1,3 +0,0 @@
-echo 	.include \"${1%.*}.s\" > tune_filename.s
-./rmac -fb ~Oall sndh.s
-./rln -z -n -a 0 x x sndh.o -o "${1%.*}.sndh"
